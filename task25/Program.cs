9263873a406bd14c816ba38fd09a1ae0a1e30d81
Andrei Lukonin <Lukonin052@gmail.com>
2022-10-28 23:14:00 +0300

    выполнил домашку

---
+++ task25/Program.cs
@@ -0,0 +1,29 @@
+﻿// Все задачи оформляем методами!! Задача 27 решается без применения строковой типизации!
+// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+void Main ()
+{
+Console.Clear();
+Console.WriteLine("Start");
+Console.WriteLine($"{Exponentiation(GetNumber(), GetNumber())}");
+Console.WriteLine("End");
+}
+int GetNumber()
+{
+Console.Write("Введите число: "); 
+return int.Parse(Console.ReadLine());
+
+}
+int Exponentiation(int NumberOne, int NumberTwo)
+{
+   int result = 1;
+   for(int i =0; i < NumberTwo; i++)
+{
+    result *= NumberOne; 
+}
+    return result;
+    
+}
+Main ();
---
+++ task29/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19, 2, 4, 9 -> [1, 2, 5, 7, 19, 2, 4, 9 ]
+// 6, 1, 33, 2, 93, 15, 424, 0-> [6, 1, 33, 2, 93, 15, 424, 0]
+
+    void showArray()
+    {
+    int[] array = new int[99998];
+    Random rand = new Random();
+    for (int i = 0; i < array.Length; i++)
+    array[i] = rand.Next(0, 9999999);
+
+    String str = "[";
+
+    foreach(int i in array){
+        str = str + i.ToString() + ", ";
+    }
+
+    str = str.Remove(str.Length - 1);
+    str = str + "]";
+
+    Console.WriteLine(str);
+}
+
+
+showArray();
